23e323f765cc70fcf2f2830cbfe58e48d990338d
Hieu C. Chu <aaazureee@gmail.com>
2017-05-13 18:27:16 +1000

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,14 +1,31 @@
-$(document).ready(function() {
-            $("#quote").click(function(e) {
-                e.preventDefault();
-                $.ajax({
-                    url: "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=",
-                    success: function(data) {
-                        var post = data.shift(); // The data is an array of posts. Grab the first one.
-                        $("#content").html("<blockquote class='blockquote'>" + post.content + "</blockquote>");
-                        $("#author").html("<em> - " + post.title + "</em>");
-                    },
-                    cache: false
-                });
-            });
+$(document).ready(function () {
+    quote();
+
+    function strip(html) {
+        var tmp = document.createElement("DIV");
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
+    }
+
+    var content = "";
+
+    function quote() {
+        $.ajax({
+            url: "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=",
+            success: function (data) {
+                var post = data.shift();
+                content = strip(post.content) + " - " + post.title + " (from @4headlul)"; // The data is an array of posts. Grab the first one.
+                $("#content").html("<blockquote class='blockquote'>" + post.content + "</blockquote>");
+                $("#author").html("<em> - " + post.title + "</em>");
+            },
+            cache: false
         });
+    }
+
+    $("#quote").click(function () {
+        quote();
+    });
+    $("#tweet").click(function () {
+        $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + content);
+    });
+});
